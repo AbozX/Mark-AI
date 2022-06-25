5c22b87599f08c55f1c056e5384df65bc39ca3d3
AbozX <85754488+AbozX@users.noreply.github.com>
2022-06-25 18:22:40 +0400

    Add files via upload

---
+++ MARK_AI/Mark.py
@@ -0,0 +1,21 @@
+import os
+
+
+print("""
+███╗   ███╗ █████╗ ██████╗ ██╗  ██╗    ███████╗███████╗████████╗██╗   ██╗██████╗ 
+████╗ ████║██╔══██╗██╔══██╗██║ ██╔╝    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+██╔████╔██║███████║██████╔╝█████╔╝     ███████╗█████╗     ██║   ██║   ██║██████╔╝
+██║╚██╔╝██║██╔══██║██╔══██╗██╔═██╗     ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+██║ ╚═╝ ██║██║  ██║██║  ██║██║  ██╗    ███████║███████╗   ██║   ╚██████╔╝██║     
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
+""")
+os.system("pip install pyttsx3")
+os.system("pip install datetime")
+os.system("pip install wikipedia")
+os.system("pip install webbrowser")
+os.system("pip install os")
+os.system("pip install random")
+os.system("pip install pyautogui")
+os.system("pip install speech_recognition")
+os.system("pip install pytube")
+os.system("python ")
